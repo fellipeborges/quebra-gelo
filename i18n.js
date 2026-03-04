aad89508ecf0c34e199d6719022faff0c7ded790
Fellipe Borges <fellipe.borges@br.imshealth.com>
2026-03-04 10:39:46 -0300

    Add language support

---
+++ i18n.js
@@ -0,0 +1,16 @@
+const ui = {
+    pt: {
+        title: "Quebra Gelo",
+        btn: "Nova pergunta",
+        hint: "ou pressione",
+        key: "espaço",
+        footer: "Código fonte"
+    },
+    en: {
+        title: "Ice Breaker",
+        btn: "New question",
+        hint: "or press",
+        key: "space",
+        footer: "Source code"
+    }
+};
